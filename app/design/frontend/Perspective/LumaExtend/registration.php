5dc1e9091c7c09033d597abb3e92e9adfc9a31c1
BogdanSuhih <sbasba92@gmail.com>
2023-12-22 16:45:15 +0200

    Theme 20: Frontend basics 2/3 (Layout)

---
+++ app/design/frontend/Perspective/LumaExtend/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME, 
+    'frontend/Perspective/LumaExtend', 
+    __DIR__
+);
